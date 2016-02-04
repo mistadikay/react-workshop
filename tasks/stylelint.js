f5ecc917b6549dee8853eada7f86842abace7afe
mistadikay <koltsov.d@gmail.com>
2016-02-04 13:12:06 +0700

    :lipstick:

--- tasks/stylelint.js
+++ tasks/stylelint.js
@@ -1,11 +1,13 @@
-export default params => input => {
+// list of available options:
+// https://github.com/stylelint/stylelint/blob/master/docs/user-guide/node-api.md#options
+export default options => input => {
     return function stylelint(log) {
         const linter = require('stylelint');
 
         return new Promise(function(resolve, reject) {
             linter.lint({
                 files: input,
-                ...params
+                ...options
             }).then(function({ errored, output }) {
                 if (errored) {
                     console.log(output);
