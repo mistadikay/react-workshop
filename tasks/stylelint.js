f34eea11d4342c34b0c17faa9f02c9df4b29552a
mistadikay <koltsov.d@gmail.com>
2016-02-04 13:27:07 +0700

    :lipstick:

--- tasks/stylelint.js
+++ tasks/stylelint.js
@@ -5,18 +5,20 @@ export default options => input => {
         const linter = require('stylelint');
 
         return new Promise(function(resolve, reject) {
-            linter.lint({
-                files: input,
-                ...options
-            }).then(function({ errored, output }) {
-                if (errored) {
-                    console.log(output);
-                    reject();
-                }
+            linter.lint({ files: input, ...options })
+                .then(function({ errored, output }) {
+                    if (errored) {
+                        console.log(output);
+                        reject();
+                    }
 
-                console.log('styles are OK');
-                resolve(input);
-            });
+                    console.log('styles are OK');
+                    resolve(input);
+                })
+                .catch(function(error) {
+                    console.log(error);
+                    reject();
+                });
         });
     };
 };
