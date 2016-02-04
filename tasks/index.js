eadf39b6ee96ba5bf233467dd338bc9b9f2401d3
mistadikay <koltsov.d@gmail.com>
2016-02-04 09:55:16 +0700

    :recycle: bump deps

--- tasks/index.js
+++ tasks/index.js
@@ -2,22 +2,29 @@ import Start from 'start';
 import logger from 'start-simple-logger';
 import clean from 'start-clean';
 import eslint from 'start-eslint';
+import files from 'start-files';
 
-import { webpackDev, webpackBuild } from './webpack';
+import * as webpack from './webpack';
 
 const start = Start(logger);
 
 export function lint() {
-    return start(eslint());
+    return start(
+        files('**/*.js'),
+        eslint()
+    );
 }
 
 export function build() {
     return start(
-        clean('build/'),
-        webpackBuild
+        files('build/'),
+        clean(),
+        webpack.build()
     );
 }
 
 export function dev() {
-    return start(webpackDev);
+    return start(
+        webpack.dev()
+    );
 }
--- tasks/webpack.js
+++ tasks/webpack.js
@@ -10,45 +10,56 @@ const host = '127.0.0.1';
 const minPort = 3000;
 const maxPort = 3010;
 
-export function webpackDev(resolve, reject) {
-    process.env.NODE_ENV = 'development';
+export const dev = params => input => {
+    return function webpackDev(log) {
+        process.env.NODE_ENV = 'development';
 
-    const webpack = require('webpack');
-    const WebpackDevServer = require('webpack-dev-server');
-    const clientOnlyDevConfig = require('../conf/webpack.dev').default;
-    const portscanner = require('portscanner');
+        const webpack = require('webpack');
+        const WebpackDevServer = require('webpack-dev-server');
+        const clientOnlyDevConfig = require('../conf/webpack.dev').default;
+        const portscanner = require('portscanner');
 
-    portscanner.findAPortNotInUse(minPort, maxPort, host, function(error, port) {
-        if (error) {
-            return reject(error);
-        }
+        return new Promise(function(resolve, reject) {
+            portscanner.findAPortNotInUse(minPort, maxPort, host, function(error, port) {
+                if (error) {
+                    return reject(error);
+                }
 
-        const server = new WebpackDevServer(webpack(clientOnlyDevConfig), {
-            hot: true,
-            stats: statsOptions
+                const server = new WebpackDevServer(webpack(clientOnlyDevConfig), {
+                    hot: true,
+                    stats: statsOptions
+                });
+
+                console.log(`http://${host}:${port}/webpack-dev-server/`);
+
+                server.listen(port, err => {
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    resolve(input);
+                });
+            });
         });
+    };
+};
 
-        server.listen(port, err => {
-            if (err) {
-                return reject(err);
-            }
+export const build = params => input => {
+    return function webpackBuild(log) {
+        process.env.NODE_ENV = 'production';
 
-            resolve(`http://${host}:${port}/webpack-dev-server/`);
+        const webpack = require('webpack');
+        const isomorphicBuildServerConfig = require('../conf/webpack.build').default;
+
+        return new Promise(function(resolve, reject) {
+            webpack(isomorphicBuildServerConfig, (err, stats) => {
+                if (err) {
+                    return reject(err);
+                }
+
+                log(stats.toString(statsOptions));
+                resolve(input);
+            });
         });
-    });
-}
-
-export function webpackBuild(resolve, reject) {
-    process.env.NODE_ENV = 'production';
-
-    const webpack = require('webpack');
-    const isomorphicBuildServerConfig = require('../conf/webpack.build').default;
-
-    webpack(isomorphicBuildServerConfig, (err, stats) => {
-        if (err) {
-            return reject(err);
-        }
-
-        resolve(stats.toString(statsOptions));
-    });
-}
+    };
+};
