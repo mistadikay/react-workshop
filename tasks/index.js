67f3f2e7e5f4f809da7919829c43ad0d0768ccf2
mistadikay <koltsov.d@gmail.com>
2016-02-22 16:32:00 +0700

    :wrench: use start-env and start-webpack

--- tasks/index.js
+++ tasks/index.js
@@ -3,11 +3,15 @@ import reporter from 'start-pretty-reporter';
 import clean from 'start-clean';
 import eslint from 'start-eslint';
 import files from 'start-files';
+import env from 'start-env';
+import * as webpack from 'start-webpack';
 
-import * as webpack from './webpack';
+import findPort from './port';
 import stylelint from './stylelint';
 import csscomb from './csscomb';
 
+const minPort = 3000;
+const maxPort = 3010;
 const start = Start(reporter());
 
 export function lint() {
@@ -32,14 +36,18 @@ export function comb() {
 
 export function build() {
     return start(
+        env('production'),
         files('build/'),
         clean(),
-        webpack.build()
+        webpack.build(require('../conf/webpack.build').default)
     );
 }
 
 export function dev() {
     return start(
-        webpack.dev()
+        env('development'),
+        findPort({ minPort, maxPort }, port => start(
+            webpack.dev(require('../conf/webpack.dev').default, port)
+        ))
     );
 }
