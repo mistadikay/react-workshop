f2373efd166fb89aa2984ac31d36adcbc6060a36
mistadikay <koltsov.d@gmail.com>
2016-03-15 11:28:38 +0700

    product-list: view from app state

--- src/components/product-list/index.js
+++ src/components/product-list/index.js
@@ -4,16 +4,8 @@ import Product from '~/components/product';
 
 export default class extends React.Component {
     render() {
-        if (this.props.view === 'grid') {
-            return (
-                <div className="product_list grid">
-                    <Product view={this.props.view} />
-                </div>
-            );
-        }
-
         return (
-            <div className="product_list list">
+            <div className={`product_list ${this.props.view}`}>
                 <Product view={this.props.view} />
             </div>
         );
