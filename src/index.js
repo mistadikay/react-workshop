a96634cfb5793c2b7e2fce8bcaef556d9b828407
mistadikay <koltsov.d@gmail.com>
2016-03-15 17:41:06 +0700

    move all styles to assets folder

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from '~/components/app';
 
+import './assets/styles.css';
+
 render(<App />, document.getElementById('app'));
