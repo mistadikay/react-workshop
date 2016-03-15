13d81ce9d5497a6c8fb99a1f101b546f7e418e47
mistadikay <koltsov.d@gmail.com>
2016-03-15 17:28:04 +0700

    BEMify search

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -3,8 +3,8 @@ import './styles.css';
 
 export default function Search(props) {
     return (
-        <div className="header__search">
-            <input className="header__search-input" type="search" {...props} />
+        <div block="search">
+            <input block="search" elem="input" type="search" {...props} />
         </div>
     );
 }
