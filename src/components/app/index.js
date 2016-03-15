180070308d0fe5b8a990160474dec4254392f9b0
mistadikay <koltsov.d@gmail.com>
2016-03-15 11:21:03 +0700

    selectedView state in app

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -7,6 +7,14 @@ import ProductList from '~/components/product-list';
 import Filter from '~/components/filter';
 
 export default class extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedView: 'grid'
+        };
+    }
+
     render() {
         return (
             <div className="catalog">
@@ -21,25 +29,10 @@ export default class extends React.Component {
                         </div>
                         <div className="catalog__status__filters">
                             <Filter />
-                            <ViewSwitcher view="grid" />
+                            <ViewSwitcher selected={this.state.selectedView} />
                         </div>
                     </div>
-                    <ProductList view="grid" />
-                </div>
-                <div className="catalog__main__content">
-                    <div className="catalog__status">
-                        <div className="catalog__status__title">
-                            <div className="component component-catalog_title">
-                                <h1 className="catalog__title">Computers &amp; Laptops</h1>
-                                <span className="catalog__quantity">18560 results</span>
-                            </div>
-                        </div>
-                        <div className="catalog__status__filters">
-                            <Filter />
-                            <ViewSwitcher view="list" />
-                        </div>
-                    </div>
-                    <ProductList view="list" />
+                    <ProductList view={this.state.selectedView} />
                 </div>
             </div>
         );
