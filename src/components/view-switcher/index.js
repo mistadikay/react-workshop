55bf0944e8c920651347a9d4c1a8ee1800c29fe4
mistadikay <koltsov.d@gmail.com>
2016-03-15 11:27:35 +0700

    view-swticher: items and selected from state

--- src/components/view-switcher/index.js
+++ src/components/view-switcher/index.js
@@ -2,36 +2,18 @@ import React from 'react';
 
 export default class extends React.Component {
     render() {
-        if (this.props.view === 'grid') {
-            return (
-                <div className="component-switcher">
-                    <span>View</span>
-                    <div className="switchers">
-                        <a href="http://www.lazada.vn/may-vi-tinh-laptop/?viewType=gridView" className="switcher selected">
-                            <i className="icon-svg catalog--view-grid"></i>
-                            Grid
-                        </a>
-                        <a href="http://www.lazada.vn/may-vi-tinh-laptop/?viewType=listView" className="switcher">
-                            <i className="icon-svg catalog--view-list"></i>
-                            List
-                        </a>
-                    </div>
-                </div>
-            );
-        }
-
         return (
             <div className="component-switcher">
                 <span>View</span>
                 <div className="switchers">
-                    <a href="http://www.lazada.vn/may-vi-tinh-laptop/?viewType=gridView" className="switcher">
-                        <i className="icon-svg catalog--view-grid"></i>
-                        Grid
-                    </a>
-                    <a href="http://www.lazada.vn/may-vi-tinh-laptop/?viewType=listView" className="switcher selected">
-                        <i className="icon-svg catalog--view-list"></i>
-                        List
-                    </a>
+                    {this.props.items.map(view => {
+                        return (
+                            <a className={'switcher' + (view === this.props.selected ? ' selected' : '')}>
+                                <i className={`icon-svg catalog--view-${view}`} key={view} />
+                                {view}
+                            </a>
+                        );
+                    })}
                 </div>
             </div>
         );
