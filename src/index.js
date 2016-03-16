730fcf5ed18412dd9be4fc97d0463aedfe17e26e
mistadikay <koltsov.d@gmail.com>
2016-03-16 18:08:25 +0700

    create store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux';
+
 import App from '#app';
 
 import './assets/styles.css';
+
+function rootReducer(state = {}, action) {
+    return state;
+}
+
+const store = createStore(rootReducer);
 
 render(<App />, document.getElementById('app'));
