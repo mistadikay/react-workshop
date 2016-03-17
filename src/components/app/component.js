6346a9c824a3c6668638fdcbd111f61a2d7d2f77
mistadikay <koltsov.d@gmail.com>
2016-03-17 16:23:20 +0700

    title from redux selector

--- src/components/app/component.js
+++ src/components/app/component.js
@@ -5,31 +5,10 @@ import ViewSwitcher from '~/components/view-switcher';
 import ProductList from '~/components/product-list';
 import CatalogStatus from '~/components/catalog-status';
 
-// a little helper to retrieve category name from server data
-function getCategoryName(data) {
-    return data.filters.find(filter => filter.id === 'category').options[0].label;
-}
-
 export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            title: ''
-        };
-    }
-
-    // let's pretend it's from the server
     componentDidMount() {
         this.props.onMount();
     }
-
-    // when data from the server is loaded, update local state
-    updateData = data => {
-        this.setState({
-            title: getCategoryName(data)
-        });
-    };
 
     render() {
         // while data is not loaded, show a loader
@@ -45,7 +24,7 @@ export default class App extends React.Component {
             <div block="app">
                 <Search placeholder="Search in the products list" />
                 <div block="app" elem="main-content">
-                    <CatalogStatus title={this.state.title} productCount={this.props.productCount}>
+                    <CatalogStatus title={this.props.title} productCount={this.props.productCount}>
                         <ViewSwitcher />
                     </CatalogStatus>
                     <ProductList data={this.props.data} />
--- src/components/app/index.js
+++ src/components/app/index.js
@@ -2,6 +2,15 @@ import { connect } from 'react-redux';
 import App from './component.js';
 import { getCatalogData } from '~/actions';
 
+// a little helper to retrieve category name from server data
+function titleSelector(data) {
+    if (data === null) {
+        return '';
+    }
+
+    return data.filters.find(filter => filter.id === 'category').options[0].label;
+}
+
 function productCountSelector(data) {
     if (data === null) {
         return 0;
@@ -13,7 +22,8 @@ function productCountSelector(data) {
 const mapStateToProps = state => {
     return {
         data: state.catalog.data,
-        productCount: productCountSelector(state.catalog.data)
+        productCount: productCountSelector(state.catalog.data),
+        title: titleSelector(state.catalog.data)
     };
 };
 
